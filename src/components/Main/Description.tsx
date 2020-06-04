1b289bad30bb742046befcafea4c0ac472658a2e
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-06-04 21:40:02 +0900

    [change] test imageの追加

--- src/components/Main/Description.tsx
+++ src/components/Main/Description.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
+/**
+ * images
+ */
+import image from '../../assets/images/image.jpg'
+
 const Conatiner = styled.div`
   padding: 4rem 0;
 `
 
-const TextContent = styled.p`
+const TextContainer = styled.p`
   text-align: center;
   margin-bottom: 4rem;
 `
@@ -19,20 +24,49 @@ const Caption = styled.p`
   margin-top: 1rem;
 `
 
-const ImageContent = styled.div`
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
 
+const ImageContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const CenterImageContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 6rem;
+`
+
+const Image = styled.img`
+width: 30vw;
 `
 
 const MainDescription: React.FC = () => {
   return (
     <Conatiner>
-      <TextContent>
+      <TextContainer>
         <Text>Support all illustrators</Text>
         <Caption>イラスト共有サイト</Caption>
-      </TextContent>
-      <ImageContent>
-        
-      </ImageContent>
+      </TextContainer>
+      <ImageContainer>
+        <ImageContent>
+          <Image src={image} />
+          <p>文言</p>
+        </ImageContent>
+        <CenterImageContent>
+          <p>文言</p>
+          <Image src={image} />
+        </CenterImageContent>
+        <ImageContent>
+          <Image src={image} />
+          <p>文言</p>
+        </ImageContent>
+      </ImageContainer>
     </Conatiner>
   )
 }
