8e496c67156732e7ac7873188bc21e7ace9b09a0
mokeeemokeee <moke.0922.moke@gmail.com>
2020-06-04 18:30:12 +0900

    [change] Description componentの追加

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,16 +4,16 @@ import styled from 'styled-components'
 /**
  * components
  */
-import MainTitle from '../components/Main/Title'
+import Title from '../components/Main/Title'
+import Description from '../components/Main/Description'
 
-const Container = styled.div`
-  
-`
+const Container = styled.div``
 
 const Main: React.FC = () => {
   return (
     <Container>
-      <MainTitle />
+      <Title />
+      <Description />
       <h1>Main</h1>
     </Container>
   )
