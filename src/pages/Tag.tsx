a9fc745149d596035e171fe2ee9ca5f97a697e3c
mokeeemokeee <moke.0922.moke@gmail.com>
2020-06-05 21:08:47 +0900

    [add] Tag(タぐ毎の表示) page追加

---
+++ src/pages/Tag.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useParams } from 'react-router-dom'
+
+const Container = styled.div``
+
+const Tag: React.FC = (props: any) => {
+  let { tag } = useParams()
+  
+  return (
+    <Container>
+      <p>Tag</p>
+      <p>{tag}</p>
+    </Container>
+  )
+}
+
+export default Tag
