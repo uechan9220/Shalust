f1a1044bc14265672d1025513ae4967fb70485bf
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-06-05 21:07:52 +0900

    [add] Home page追加

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <p>Home</p>
+    </Container>
+  )
+}
+
+export default Home
