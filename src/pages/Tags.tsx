d305062d2aeaf36ccf3cec028660cb039fde5d96
mokeeemokeee <moke.0922.moke@gmail.com>
2020-06-05 21:08:29 +0900

    [add] Tags(タぐ一覧表示) page追加

---
+++ src/pages/Tags.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+const Tags: React.FC = () => {
+  return (
+    <Container>
+      <p>Tags</p>
+    </Container>
+  )
+}
+
+export default Tags
