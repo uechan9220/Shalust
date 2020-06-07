3a26cc6a5d2dce89c3d64c6f91117d9aa9daa72d
mokeeemokeee <moke.0922.moke@gmail.com>
2020-06-08 01:52:56 +0900

    [chnage] routerに追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import * as BaseTheme from './themes/BaseStyle'
 import reset from 'styled-reset'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import styled from 'styled-components'
+
+/**
+ * Header
+ */
+import Header from './components/Header'
 
 /**
  * pages
@@ -16,10 +18,16 @@ import Tag from './pages/Tag'
 import Tags from './pages/Tags'
 import MyPage from './pages/MyPage'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signin from './pages/Signin'
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
   /* other styles */
+`
+
+const Container = styled.div`
+  padding-top: 4.2rem;
 `
 
 function App() {
@@ -27,23 +35,32 @@ function App() {
     <ThemeProvider theme={BaseTheme}>
       <GlobalStyle />
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route path="/mypage/:id">
-            <MyPage />
-          </Route>
-          <Route exact path="/tags">
-            <Tags />
-          </Route>
-          <Route path="/tags/:tag">
-            <Tag />
-          </Route>
-        </Switch>
+        <Header />
+        <Container>
+          <Switch>
+            <Route exact path="/">
+              <Main />
+            </Route>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route path="/mypage/:id">
+              <MyPage />
+            </Route>
+            <Route exact path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/tags/:tag">
+              <Tag />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signin">
+              <Signin />
+            </Route>
+          </Switch>
+        </Container>
       </Router>
     </ThemeProvider>
   )
