d47514dc459d0d8acc2e34517451165b26db3875
mokeeemokeee <moke.0922.moke@gmail.com>
2020-06-08 01:52:05 +0900

    [add] page

---
+++ src/pages/Signin.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+const Signin: React.FC = () => {
+  return (
+    <Container>
+      <p>Signin</p>
+    </Container>
+  )
+}
+
+export default Signin
