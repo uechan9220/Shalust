47fa174c6e6877c1253b43e47da8be9a839c3fe6
MokeeeMokee <moke.0922.moke@gmail.com>
2020-06-08 01:52:16 +0900

    [add] page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+const Login: React.FC = () => {
+  return (
+    <Container>
+      <p>Login</p>
+    </Container>
+  )
+}
+
+export default Login
