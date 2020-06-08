b81d207a2252e51ac54889b95141440605144a5a
MokeeeMokee <moke.0922.moke@gmail.com>
2020-06-08 18:09:24 +0900

    [fix]

---
+++ src/Auth.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef, createContext } from 'react'
+import { Redirect } from 'react-router-dom'
+import firebase from './Firebase'
+import { User } from 'firebase'
+
+// import LoadingOverlay from 'react-loading-overlay'
+
+// Contextの型を用意
+interface IAuthContext {
+  currentUser: User | null | undefined
+}
+
+// Contextを宣言。Contextの中身を {currentUser: undefined} と定義
+const AuthContext = createContext<IAuthContext>({
+  currentUser: undefined,
+})
+
+const AuthProvider = (props: any) => {
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>(
+    undefined
+  )
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user)
+    })
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ currentUser: currentUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
+
+export { AuthContext, AuthProvider }
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import styled from 'styled-components'
+import firebase from '../Firebase'
+import { AuthContext } from '../Auth'
 
 const Container = styled.div``
 
-const Signin: React.FC = () => {
+const Signin: React.FC = (props: any) => {
+  const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    console.log(currentUser)
+    firebase.auth().onAuthStateChanged((user: any) => {
+      currentUser && props.history.push('/')
+    })
+  }, [currentUser])
+
+  const login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
+  }
+
   return (
     <Container>
       <p>Signin</p>
+      <button onClick={() => login()}>Login</button>
     </Container>
   )
 }
