643420fc9cf7b5cd034f1bb6dea77cfd5b798e75
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-06-11 15:44:47 +0900

    [change] JWT認証できるように変更

--- src/AuthProvider.tsx
+++ src/AuthProvider.tsx
@@ -1,29 +1,58 @@
 import React, { useState, useEffect, useRef, createContext } from 'react'
 import { Redirect } from 'react-router-dom'
-import firebase from './Firebase'
+import firebase  from './Firebase'
 import { User } from 'firebase'
 
 // import LoadingOverlay from 'react-loading-overlay'
 
 // Contextの型を用意
 interface IAuthContext {
-  currentUser: User | null | undefined
+  currentUser: AuthProps
 }
 
 // Contextを宣言。Contextの中身を {currentUser: undefined} と定義
 const AuthContext = createContext<IAuthContext>({
-  currentUser: undefined,
+  currentUser: { status: 'loading' },
 })
 
+interface AuthProps {
+  status: string
+  user?: User
+  token?: string
+}
+
 const AuthProvider = (props: any) => {
-  const [currentUser, setCurrentUser] = useState<User | null | undefined>(
-    undefined
-  )
+  const [currentUser, setCurrentUser] = useState<AuthProps>({
+    status: 'loading',
+  })
 
   useEffect(() => {
     // if (currentUser === undefined) history.push('/signin')
-    firebase.auth().onAuthStateChanged((user) => {
-      setCurrentUser(user)
+    firebase.auth().onAuthStateChanged(async (user: any) => {
+      if (user) {
+        const token = await user.getIdToken()
+        const idTokenResult = await user.getIdTokenResult()
+        const hasuraClaim = idTokenResult.claims['https://hasura.io/jwt/claims']
+        const db = firebase.database()
+        console.log(user)
+        console.log(token)
+        console.log(hasuraClaim)
+
+        if (hasuraClaim) {
+          setCurrentUser({ status: 'in', user, token })
+        } else {
+          // Check if refresh is required.
+          const metadataRef = db.ref('metadata/' + user.uid + '/refreshTime')
+
+          metadataRef.on('value', async () => {
+            // Force refresh to pick up the latest custom claims changes.
+            const token = await user.getIdToken(true)
+            setCurrentUser({ status: 'in', user, token })
+          })
+        }
+      } else {
+        setCurrentUser({ status: 'out' })
+      }
     })
   }, [])
 
