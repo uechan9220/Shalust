86abb45cd6e84843de2ea469fb87e7fcf0fbeeca
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-06-16 21:55:07 +0900

    [change]  postQuery 追加

--- src/pages/Tag.tsx
+++ src/pages/Tag.tsx
@@ -1,16 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
+import { Query, QueryResult } from 'react-apollo'
+
+/**
+ * query, Props
+ */
+import { allPostQuery } from '../data/queries'
+import { PostQueryProps } from '../generated/Props'
 
 const Container = styled.div``
 
+const Title = styled.p`
+  text-align: center;
+  margin: 2rem 0;
+  font-size: 3rem;
+  font-weight: bold;
+`
+
 const Tag: React.FC = (props: any) => {
   let { tag } = useParams()
-  
+
   return (
-    <Container>
-      <p>{tag}</p>
-    </Container>
+    <Query query={allPostQuery} variables={{ tagName: '期待の新人' }}>
+      {({ loading, data, error }: QueryResult<PostQueryProps>) => {
+        return (
+          <Container>
+            <Title>{tag}</Title>
+            {console.log(data)}
+            {!loading && data
+              ? data.Post.map((items: any, index: number) => {
+                  return (
+                    <div>
+                      <p>{items.id}</p>
+                      <p>{items.create_at}</p>
+                      <p>{items.caption}</p>
+                      <p>{items.image}</p>
+                    </div>
+                  )
+                })
+              : null}
+          </Container>
+        )
+      }}
+    </Query>
   )
 }
 
