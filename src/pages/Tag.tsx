2e96e0b0a75a777b153a614ee85f4dc4e816a926
mokeeemokeee <moke.0922.moke@gmail.com>
2020-06-18 21:30:11 +0900

    [change] style, likeCount

--- src/pages/Tag.tsx
+++ src/pages/Tag.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
-import { Query, QueryResult } from 'react-apollo'
+import { Query, QueryResult, useMutation } from 'react-apollo'
+import { AuthContext } from '../AuthProvider'
 
 /**
  * query, Props
  */
 import { allPostQuery } from '../data/queries'
 import { PostQueryProps } from '../generated/Props'
+import { likePost } from '../data/mutation'
+
+/**
+ * どうにかstyled-componentsで実装したい
+ * 参考URL -> https://qiita.com/wintyo/items/cb90e5ac50d193cce578
+ */
+// @mixin flex-fitting($column - width, $max - column) {
+//   @for $i from 1 through $max - column {
+//     @media only screen and(min - width: $i * $item - width) {
+//       max - width: $i * $item - width;
+//     }
+//   }
+// }
 
 const Container = styled.div`
   padding: 0 1rem;
@@ -22,19 +36,34 @@ const Title = styled.p`
 
 const ContentContainer = styled.div`
   display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  &::after {
+    content: '';
+    flex: auto;
+  }
 `
 
 const Content = styled.div`
-  width: 15rem;
-  height: 15rem;
+  min-width: 15rem;
+  min-height: 15rem;
+  max-width: 15rem;
+  max-height: 15rem;
+  margin: 2rem 1rem;
 `
 
 const ContentImage = styled.img`
   width: 100%;
+  height: 100%;
 `
 
 const Tag: React.FC = (props: any) => {
   let { tag } = useParams()
+  const { currentUser } = useContext(AuthContext)
+
+  const [like_post] = useMutation(likePost, {
+    // variables: {postId: }
+  })
 
   return (
     <Query query={allPostQuery} variables={{ tagName: tag }}>
@@ -50,6 +79,7 @@ const Tag: React.FC = (props: any) => {
                         <ContentImage src={items.image} />
                         <p>{items.create_at}</p>
                         <p>{items.caption}</p>
+                        <p>Like: {items.Likes_aggregate.aggregate.count}</p>
                       </Content>
                     )
                   })
