134f3f801a85da51d9b1b58adb0a39bd052d2299
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-07-13 18:40:25 +0900

    [add] READMEの編集

--- api/cmd/local_development.go
+++ api/cmd/local_development.go
@@ -3,11 +3,15 @@ package main
 import (
 	"shalust/api/pkg/server"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000/"}
+	r.Use(cors.New(config))
 
 	server.Serve(r, ":8080")
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/aws/aws-sdk-go v1.33.1 // indirect
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.14
 	github.com/rubenv/sql-migrate v0.0.0-20200616145509-8d140a17f351 // indirect
