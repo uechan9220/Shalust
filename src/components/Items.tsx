4b6a9a04e8eb79603a9d80ec46ffd464a1bf11cd
mokeeemokeee <moke.0922.moke@gmail.com>
2020-07-17 18:02:17 +0900

    [change] Data構造

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -17,87 +17,122 @@ const Items: React.FC = () => {
   const Data = [
     {
       id: 1,
-      image: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+      image_url:
+        'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
       title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
-      userImage:
-        'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
-      userName: 'fuga',
       comment: 22,
       like: 22,
       isLike: false,
       isBookmark: true,
+      User: [
+        {
+          userImage:
+            'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+          userName: 'fuga',
+        },
+      ],
     },
     {
       id: 2,
-      image: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+      image_url:
+        'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
       title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
-      userImage:
-        'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
-      userName: 'moke',
       comment: 22,
       like: 23,
       isLike: true,
       isBookmark: false,
+      User: [
+        {
+          userImage:
+            'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+          userName: 'moke',
+        },
+      ],
     },
     {
       id: 3,
-      image: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+      image_url:
+        'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
       title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
-      userImage:
-        'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
-      userName: 'moke',
       comment: 22,
       like: 23,
       isLike: true,
       isBookmark: false,
+      User: [
+        {
+          userImage:
+            'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+          userName: 'moke',
+        },
+      ],
     },
     {
       id: 4,
-      image: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+      image_url:
+        'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
       title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
-      userImage:
-        'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
-      userName: 'moke',
       comment: 22,
       like: 23,
       isLike: true,
       isBookmark: false,
+      User: [
+        {
+          userImage:
+            'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+          userName: 'moke',
+        },
+      ],
     },
     {
       id: 5,
-      image: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+      image_url:
+        'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
       title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
-      userImage:
-        'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
-      userName: 'moke',
       comment: 22,
       like: 23,
       isLike: true,
       isBookmark: false,
+      User: [
+        {
+          userImage:
+            'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+          userName: 'moke',
+        },
+      ],
     },
     {
       id: 6,
-      image: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+      image_url:
+        'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
       title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
-      userImage:
-        'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
-      userName: 'moke',
       comment: 22,
       like: 23,
       isLike: true,
       isBookmark: false,
+      User: [
+        {
+          userImage:
+            'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+          userName: 'moke',
+        },
+      ],
     },
     {
       id: 7,
-      image: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+      image_url:
+        'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
       title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
-      userImage:
-        'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
-      userName: 'moke',
       comment: 22,
       like: 23,
       isLike: true,
       isBookmark: false,
+      User: [
+        {
+          userImage:
+            'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+          userName: 'moke',
+        },
+      ],
     },
   ]
 
