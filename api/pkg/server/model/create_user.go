5298bf2cc6b33bcaf40fd34d92eec6c6cf9c0c02
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-07-21 21:16:51 +0900

    [add]サンプルの挿入

--- api/pkg/server/model/create_user.go
+++ api/pkg/server/model/create_user.go
@@ -10,12 +10,9 @@ import (
 func CreateUser(data User) {
 	fmt.Println(data)
 	client, err := db.Init_mysql()
-	var hoge User
 	if err != nil {
 		fmt.Println(err)
 	}
-	client.NewRecord(&data)
 	client.From("user").Create(&data)
-	client.From("user").Find(&hoge)
-	fmt.Println(hoge)
+
 }
