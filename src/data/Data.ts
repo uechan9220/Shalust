6ea54da454d4ca4717b93c91e0b8d3d9e245ad4d
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-07-22 21:27:35 +0900

    [add] 仮データ

---
+++ src/data/Data.ts
@@ -0,0 +1,468 @@
+import React from 'react'
+
+/**
+ * test Image
+ */
+import Rough from '../assets/images/rough.png'
+import Commic from '../assets/images/Commic.jpg'
+import Graffiti from '../assets/images/Graffiti.jpg'
+
+export const IllustData = [
+  {
+    id: 1,
+    image_url: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+    title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
+    comment: 22,
+    like: 22,
+    isLike: false,
+    isBookmark: true,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'fuga',
+      },
+    ],
+  },
+  {
+    id: 2,
+    image_url: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+    title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
+    comment: 22,
+    like: 23,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 3,
+    image_url: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+    title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
+    comment: 22,
+    like: 23,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 4,
+    image_url: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+    title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
+    comment: 22,
+    like: 23,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 5,
+    image_url: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+    title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
+    comment: 22,
+    like: 23,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 6,
+    image_url: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+    title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
+    comment: 22,
+    like: 23,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 7,
+    image_url: 'https://ca.slack-edge.com/TH9SKRH3N-UH7F80B6C-c0365f443d8f-512',
+    title: 'モーグリーとロアちゃんが二人でお昼中なのだ',
+    comment: 22,
+    like: 23,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+]
+
+export const RoughData = [
+  {
+    id: 1,
+    image_url: Rough,
+    title: '女の子がわちゃわちゃといい感じのラフです',
+    comment: 10,
+    like: 50,
+    isLike: false,
+    isBookmark: true,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'fuga',
+      },
+    ],
+  },
+  {
+    id: 2,
+    image_url: Rough,
+    title: '女の子がわちゃわちゃといい感じのラフです',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 3,
+    image_url: Rough,
+    title: '女の子がわちゃわちゃといい感じのラフです',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 4,
+    image_url: Rough,
+    title: '女の子がわちゃわちゃといい感じのラフです',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 5,
+    image_url: Rough,
+    title: '女の子がわちゃわちゃといい感じのラフです',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 6,
+    image_url: Rough,
+    title: '女の子がわちゃわちゃといい感じのラフです',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 7,
+    image_url: Rough,
+    title: '女の子がわちゃわちゃといい感じのラフです',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+]
+
+export const CommicData = [
+  {
+    id: 1,
+    image_url: Commic,
+    title: 'Comic backgrounds Free Vector',
+    comment: 10,
+    like: 50,
+    isLike: false,
+    isBookmark: true,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'fuga',
+      },
+    ],
+  },
+  {
+    id: 2,
+    image_url: Commic,
+    title: 'Comic backgrounds Free Vector',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 3,
+    image_url: Commic,
+    title: 'Comic backgrounds Free Vector',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 4,
+    image_url: Commic,
+    title: 'Comic backgrounds Free Vector',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 5,
+    image_url: Commic,
+    title: 'Comic backgrounds Free Vector',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 6,
+    image_url: Commic,
+    title: 'Comic backgrounds Free Vector',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 7,
+    image_url: Commic,
+    title: 'Comic backgrounds Free Vector',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+]
+
+export const GraffitiData = [
+  {
+    id: 1,
+    image_url: Graffiti,
+    title: 'しょうこおねーさんのすぷーを書いてみました!!',
+    comment: 10,
+    like: 50,
+    isLike: false,
+    isBookmark: true,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'fuga',
+      },
+    ],
+  },
+  {
+    id: 2,
+    image_url: Graffiti,
+    title: 'しょうこおねーさんのすぷーを書いてみました!!',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 3,
+    image_url: Graffiti,
+    title: 'しょうこおねーさんのすぷーを書いてみました!!',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 4,
+    image_url: Graffiti,
+    title: 'しょうこおねーさんのすぷーを書いてみました!!',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 5,
+    image_url: Graffiti,
+    title: 'しょうこおねーさんのすぷーを書いてみました!!',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 6,
+    image_url: Graffiti,
+    title: 'しょうこおねーさんのすぷーを書いてみました!!',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+  {
+    id: 7,
+    image_url: Graffiti,
+    title: 'しょうこおねーさんのすぷーを書いてみました!!',
+    comment: 10,
+    like: 44,
+    isLike: true,
+    isBookmark: false,
+    User: [
+      {
+        userImage:
+          'https://ca.slack-edge.com/T0G4VRKJA-U51V91129-5340348de13a-512',
+        userName: 'moke',
+      },
+    ],
+  },
+]
