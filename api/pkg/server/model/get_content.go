82d1ba01e7cd31629f3a0b8ca89ed8237a948382
rin1208 <sora1208sky@gmail.com>
2020-07-30 22:11:35 +0900

    [add]sampleデータの取得

--- api/pkg/server/model/get_content.go
+++ api/pkg/server/model/get_content.go
@@ -3,19 +3,12 @@ package model
 import (
 	"log"
 	"shalust/api/pkg/db"
-	"time"
 )
 
 type TestData struct {
-	Content_id  string    `json:"content_id"`
-	User_id     string    `json:"user_id"`
-	Detail      string    `json:"detail"`
-	Create_at   time.Time `json:"create_at"`
-	Title       string    `json:"title"`
-	Views       int       `json:"views"`
-	Adult       bool      `json:"adult"`
-	Image_url   string    `json:"image_url"`
-	Image_index int       `json:"image_index"`
+	ContentHandling
+	Image_url   string
+	Image_index int
 }
 
 func GetAllIllustratio() ([]TestData, error) {
@@ -28,8 +21,7 @@ func GetAllIllustratio() ([]TestData, error) {
 
 	// clientt := client.From("content_handling").Select("content_handling.*, illustratio.image_url, illustratio.image_index")
 
-	clientt := client.From("content_handling").Select("content_handling.content_id" +
-		", illustratio.image_url, illustratio.image_index")
+	clientt := client.From("content_handling, illustratio").Select("content_handling.*, illustratio.image_url, illustratio.image_index")
 
 	clientt.Join("JOIN illustratio ON illustratio.content_id = content_handling.content_id").Scan(&data)
 
