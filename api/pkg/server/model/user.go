15453b6ae52f274f1342a90c3693d49ad515743a
rin1208 <sora1208sky@gmail.com>
2020-08-07 00:30:58 +0900

    [add]userの取得

--- api/pkg/server/model/user.go
+++ api/pkg/server/model/user.go
@@ -5,18 +5,22 @@ import (
 )
 
 func CreateUser(data User) error {
+
 	client, err := db.Init_mysql()
 	if err != nil {
 		return err
 	}
 	client.From("user").Create(&data)
+	defer client.Close()
 	return err
 }
 func GetUserData(user_id string, data *User) error {
+
 	client, err := db.Init_mysql()
 	if err != nil {
 		return err
 	}
 	client.From("user").Where("user_id = ?", user_id).Scan(data)
+	defer client.Close()
 	return err
 }
