335d03803c1079ac71c6e6469105a15f31dac72d
rin1208 <sora1208sky@gmail.com>
2020-08-12 15:42:37 +0900

    [add]データ取得方法の追加

--- api/pkg/server/model/get_content.go
+++ api/pkg/server/model/get_content.go
@@ -13,14 +13,15 @@ func GetAllIllustratio(data *[]ContentData) error {
 
 	client.
 		From("content_handling").
-		Select(`content_handling.content_id, user.user_name, user.icon_url`).
-		// Join("JOIN content_data ON content_data.content_id = content_handling.content_id").
+		Select(`content_handling.*, user.user_name, user.icon_url, content_data.image_url, content_data.image_index, likes.user_liked, bookmarks.user_bookmarked`).
+		Join("JOIN content_data ON content_data.content_id = content_handling.content_id").
 		Join("JOIN user ON user.user_id = content_handling.user_id").
-		// Join("JOIN likes ON likes.content_id = content_handling.content_id AND likes.user_id = ?", "testid2").
-		// Where("illustratio = 1").
+		Join("LEFT JOIN likes ON likes.content_id = content_handling.content_id AND likes.user_id = ?", "testid").
+		Join("LEFT JOIN bookmarks ON bookmarks.content_id = content_handling.content_id AND bookmarks.user_id = ?", "testid").
+		Where("illustratio = 1").
 		Scan(data)
 
-	// defer client.Close()
+	defer client.Close()
 	return err
 }
 
