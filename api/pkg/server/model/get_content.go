b7d5d4def4e9554a96c12bc9a9090649a60f88a6
rin1208 <sora1208sky@gmail.com>
2020-08-12 15:44:01 +0900

    [add]データ取得方法の追加

--- api/pkg/server/model/get_content.go
+++ api/pkg/server/model/get_content.go
@@ -32,9 +32,12 @@ func GetUserIllustratio(user_id string, data *[]ContentData) error {
 		return err
 	}
 
-	client.From("content_handling, content_data, user").
+	client.From("content_handling").
+		Select(`content_handling.*, user.user_name, user.icon_url, content_data.image_url, content_data.image_index, likes.user_liked, bookmarks.user_bookmarked`).
 		Join("JOIN content_data ON content_data.content_id = content_handling.content_id").
 		Join("JOIN user ON user.user_id = content_handling.user_id").
+		Join("LEFT JOIN likes ON likes.content_id = content_handling.content_id AND likes.user_id = ?", "testid").
+		Join("LEFT JOIN bookmarks ON bookmarks.content_id = content_handling.content_id AND bookmarks.user_id = ?", "testid").
 		Where("content_handling.user_id = ?", user_id).
 		Where("illustratio = 1").
 		Scan(data)
