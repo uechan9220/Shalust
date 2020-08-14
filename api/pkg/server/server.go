13f03e2ff4548b885d36b25cd7bdc662c0c3d5a2
rin1208 <sora1208sky@gmail.com>
2020-08-14 18:28:47 +0900

    [update]api下に配置

--- api/pkg/server/server.go
+++ api/pkg/server/server.go
@@ -11,14 +11,13 @@ func Serve(r *gin.Engine, port string) {
 	r.GET("/", handler.Test)
 	r.GET("/test", handler.GetIllustratio)
 
-	r.GET("/user/:user_id", handler.GetUserIllustratio)
-	r.GET("/user/:user_id/Illustratio", handler.GetUserIllustratio)
-	r.GET("/user/:user_id/commic", handler.GetUserCommic)
-	r.GET("/user/:user_id/graffiti", handler.GetUserGraffiti)
-	r.GET("/user/:user_id/rough", handler.GetUserRough)
 	v1 := r.Group("/api")
 	{
-
+		v1.GET("/user/:user_id", handler.GetUserIllustratio)
+		v1.GET("/user/:user_id/Illustratio", handler.GetUserIllustratio)
+		v1.GET("/user/:user_id/commic", handler.GetUserCommic)
+		v1.GET("/user/:user_id/graffiti", handler.GetUserGraffiti)
+		v1.GET("/user/:user_id/rough", handler.GetUserRough)
 		v1.POST("/getIllustratio", handler.GetIllustratio)
 		v1.POST("/createUser", handler.CreateUser)
 
