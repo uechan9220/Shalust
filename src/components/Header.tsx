b448f12ef3df540d9540fe1b1143c8d14adf6af2
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-08-16 23:51:14 +0900

    [change] 仮リンク先

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -73,12 +73,13 @@ const Header: React.FC = () => {
       <Link to="/">
         <Title>Illustgram</Title>
       </Link>
+      {console.log(currentUser)}
       {currentUser.user ? (
         <Content>
           <Link to="/tags">
             <Signin>Tags</Signin>
           </Link>
-          {data.User.map((items: any, index: number) => {
+          {/* {data.User.map((items: any, index: number) => {
             return (
               // test Link
               // <Link to={`/user/${items.uniqueID}`}>
@@ -86,7 +87,10 @@ const Header: React.FC = () => {
                 <Signin>user</Signin>
               </Link>
             )
-          })}
+          })} */}
+          <Link to={`/user/moooooooooooke`}>
+            <Signin>user</Signin>
+          </Link>
         </Content>
       ) : (
           <Content>
