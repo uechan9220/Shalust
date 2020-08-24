f11b33b49d7ab8120664fe02684a3a640d4db8b6
rin1208 <sora1208sky@gmail.com>
2020-08-24 17:01:47 +0900

    [add]名前解決

--- api/cmd/local_development.go
+++ api/cmd/local_development.go
@@ -5,11 +5,13 @@ import (
 	"time"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile("/app/build", true)))
 	r.Use(cors.New(cors.Config{
 		// 許可したいHTTPメソッドの一覧
 		AllowMethods: []string{
