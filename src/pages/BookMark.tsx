692f24c568f4c5e404e0cb084890193d9ed17e25
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-09-23 23:19:38 +0900

    [add] BookMark 追加

---
+++ src/pages/BookMark.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const BookMark: React.FC = () => {
+  return (
+    <Container>
+      <p>bookmark</p>
+    </Container>
+  );
+};
+
+export default BookMark;
