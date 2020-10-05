1996f1a751cc6fb79a8ee2c369cd0a849944f020
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-10-05 13:34:38 +0900

    [change] components

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,54 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 /**
  * components
  */
-import Items from '../components/Items'
-import Navbar from '../components/Navbar'
+import TopSection from '../components/Main/TopSection';
 
 /**
  * testData
  */
-import { IllustDataTest } from '../data/Data'
-import { Query } from 'react-apollo'
-import { GetIllustQuery } from '../data/queries'
 
-const Container = styled.div`
-  min-height: 80vh; /* 後で消す */
-  padding: 2rem 1rem;
-  background-color: #e6ecf0;
-`
-const Content = styled.div`
-  padding: 2rem 1rem;
-  background-color: #fff;
-`
+const Container = styled.div``;
+const Content = styled.div``;
 
 const Main: React.FC = () => {
   return (
-    // <Query query={GetIllustQuery}>
-    //   {({ loading, data, err }: any) => {
-    //     if (err) console.log(err)
-    //     if (loading) { return <p>...loading</p> }
-    //     console.log(IllustDataTest)
-
-    //     return (
-    //       <Container>
-    //         <Content>
-    //           <Navbar selectNumber={1} />
-    //           <Items datas={data.illust} isInfo={true} />
-    //         </Content>
-    //       </Container>
-    //     )
-    //   }}
-    // </Query>
     <Container>
-      <Content>
-        <Navbar selectNumber={1} />
-        <Items datas={IllustDataTest} isInfo={true} />
-      </Content>
+      <TopSection />
     </Container>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
