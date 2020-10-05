27ea3acd1c925be1388fccdf5ba1b145024b8562
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-10-06 00:14:15 +0900

    [chnage] style

--- src/pages/Illustratio.tsx
+++ src/pages/Illustratio.tsx
@@ -1,50 +1,50 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-import { GetIllustQuery } from '../data/queries'
+import { GetIllustQuery } from '../data/queries';
 
 /**
  * components
  */
-import Items from '../components/Items'
-import Navbar from '../components/Navbar'
+import Items from '../components/Items';
+import Navbar from '../components/Navbar';
 
 /**
  * testData
  */
-import { Query, Mutation } from 'react-apollo'
-import { IllustDataTest } from '../data/Data'
+import { Query, Mutation } from 'react-apollo';
+import { IllustDataTest } from '../data/Data';
 
 const Container = styled.div`
   min-height: 80vh; /* 後で消す */
   padding: 2rem 1rem;
   background-color: #e6ecf0;
-`
+`;
 const Content = styled.div`
   padding: 2rem 1rem;
   background-color: #fff;
-`
+`;
 
 const Illust: React.FC = () => {
   return (
     <Query query={GetIllustQuery}>
       {({ loading, data, err }: any) => {
-        console.log(err)
-        if (loading) { return <p>...loading</p> }
-        console.log(data.illust)
-        console.log(IllustDataTest)
+        console.log(err);
+        if (loading) {
+          return <p>...loading</p>;
+        }
+        console.log(IllustDataTest);
 
         return (
           <Container>
             <Content>
               <Navbar selectNumber={1} />
-              <Items datas={data.illust} isInfo={true} path={"illustratio"} />
+              <Items datas={data.illust} isInfo={true} path={'illustratio'} />
             </Content>
           </Container>
-        )
+        );
       }}
-
-    </Query >
+    </Query>
     // <Container>
     //   <Content>
     //     <Navbar selectNumber={1} />
@@ -52,7 +52,7 @@ const Illust: React.FC = () => {
     //     <Items datas={IllustDataTest} isInfo={true} />
     //   </Content>
     // </Container>
-  )
-}
+  );
+};
 
-export default Illust
+export default Illust;
