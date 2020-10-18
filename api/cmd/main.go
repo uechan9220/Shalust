c491a70f0fd777a182c6963f0c8fafc520a07dbc
rin1208 <sora1208sky@gmail.com>
2020-10-19 02:46:57 +0900

    [change]ファイルの修正

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"shalust/api/pkg"
 
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-
+	r.Use(static.Serve("/", static.LocalFile("./build", true)))
 	pkg.Serve(r, ":8080")
 }
