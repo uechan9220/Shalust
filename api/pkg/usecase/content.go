074420e0ff9aafc30ee4125d9af797b4faecf36d
rin1208 <sora1208sky@gmail.com>
2020-10-21 00:49:20 +0900

    likenの部分の修正

--- api/pkg/usecase/content.go
+++ api/pkg/usecase/content.go
@@ -36,12 +36,25 @@ func DeleteContent(content_id string) error {
 func LikeContent(requestData model.UserLikeContent) error {
 
 	client, err := infra.Init_mysql()
+	defer client.Close()
 	if err != nil {
 		return err
 	}
 	requestData.User_liked = true
+
+	var data model.UserLikeContent
+	client.From("likes").
+		Where("user_id = ?", requestData.User_id).
+		Where("content_id = ?", requestData.Content_id).
+		Scan(&data)
+
+	if data.User_liked {
+		client.Exec("DELETE FROM likes WHERE content_id = ? AND user_id = ? ", requestData.Content_id, requestData.User_id)
+		return err
+	}
+
 	client.From("likes").Create(&requestData)
-	defer client.Close()
+
 	return err
 }
 func BookmarkContent(requestData model.UserBookmarkContent) error {
@@ -51,6 +64,18 @@ func BookmarkContent(requestData model.UserBookmarkContent) error {
 		return err
 	}
 	requestData.User_bookmarked = true
+
+	var data model.UserBookmarkContent
+	client.From("bookmarks").
+		Where("user_id = ?", requestData.User_id).
+		Where("content_id = ?", requestData.Content_id).
+		Scan(&data)
+
+	if data.User_bookmarked {
+		client.Exec("DELETE FROM bookmarks WHERE content_id = ? AND user_id = ? ", requestData.Content_id, requestData.User_id)
+		return err
+	}
+
 	client.From("bookmarks").Create(&requestData)
 	defer client.Close()
 	return err
