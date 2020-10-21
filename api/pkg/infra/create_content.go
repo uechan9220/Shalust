1edf34a5f88a83c5e48e22dfb9df9df6e0f88f52
rin1208 <sora1208sky@gmail.com>
2020-10-21 11:41:56 +0900

    依存関係の修正

--- api/pkg/infra/create_content.go
+++ api/pkg/infra/create_content.go
@@ -1,8 +1,6 @@
 package infra
 
 import (
-	"fmt"
-	"regexp"
 	"shalust/api/pkg/model"
 )
 
@@ -75,82 +73,4 @@ func CreateContent(data model.Content) error {
 	client.From("content_data").Create(&data)
 	defer client.Close()
 	return err
-}
-
-func PostContent(imageData []model.Images, contentId string) error {
-	var images []model.Content
-
-	for _, v := range imageData {
-		var data model.Content
-		var url string
-		match, _ := regexp.MatchString("jpeg", v.Image)
-		if match {
-			url, _ = SaveContentImage(v.Image[23:], contentId, v.Index)
-		} else {
-			url, _ = SaveContentImage(v.Image[22:], contentId, v.Index)
-		}
-		data.Image_url = url
-		data.Image_index = v.Index
-		data.Content_id = contentId
-		_ = CreateContent(data)
-		images = append(images, data)
-	}
-	return nil
-}
-
-func PostContentHandling(data model.PostContentData, contentId string) error {
-	var content_handling model.ContentHandling
-
-	content_handling.Disclose = true
-
-	content_handling.Create_at = GetDeteInTokyo()
-	content_handling.Content_id = contentId
-	content_handling.Title = data.Title
-	content_handling.User_id = data.User_id
-	content_handling.Detail = data.Detail
-	content_handling.Adult = data.Adult
-
-	thumbnail_url, _ := CreateThumbail(data.Images[data.ThumbailNumber-1].Image, contentId)
-
-	content_handling.Thumbnail_url = thumbnail_url
-	if data.Illustratio {
-		err := CreateIllustratioHandling(content_handling)
-		if err != nil {
-			return err
-		}
-	} else if data.Graffiti {
-		err := CreateGraffitiHandling(content_handling)
-		if err != nil {
-			return err
-		}
-	} else if data.Rough {
-		err := CreateRoughtHandling(content_handling)
-		if err != nil {
-			return err
-		}
-	} else if data.Commic {
-		err := CreateCommicHandling(content_handling)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func CreateThumbail(image, contentId string) (string, error) {
-	match, _ := regexp.MatchString("jpeg", image)
-	if match {
-		url, _ := SaveThumbailImage(image[23:], contentId)
-		return url, nil
-	}
-	url, _ := SaveThumbailImage(image[22:], contentId)
-	return url, nil
-
-}
-
-func CreateTag(tagData []string) error {
-	for _, v := range tagData {
-		fmt.Println(v)
-	}
-	return nil
 }
--- api/pkg/infra/sql_handler.go
+++ api/pkg/infra/sql_handler.go
@@ -1,4 +1,24 @@
 package infra
 
+import "github.com/jinzhu/gorm"
+
 type SqlHandler interface {
+	Find(interface{}) *gorm.DB
+	Scan(interface{}) *gorm.DB
+	Where(string, ...interface{}) *gorm.DB
+	Close()
+	Create(interface{}) *gorm.DB
+	Delete(interface{}, ...interface{}) *gorm.DB
+	Having(interface{}) *gorm.DB
+	Offset(interface{}) *gorm.DB
+	Limit(interface{}) *gorm.DB
+	From(string) *gorm.DB
+	Join(string, ...interface{}) *gorm.DB
+	Select(interface{}, ...interface{}) *gorm.DB
+	Rows() *gorm.DB
+	Preload(string, ...interface{}) *gorm.DB
+	Exec(string, ...interface{}) *gorm.DB
+	Update(...interface{}) *gorm.DB
+	Updates(interface{}) *gorm.DB
+	Error(interface{}) *gorm.DB
 }
